20a9ea628cad737ffde71dfaf0d4ce9fb34a7779
AAlicante <alicanteallan7@gmail.com>
2021-07-20 17:39:59 +0800

    added modal to teacher list form

--- teacher_list.php
+++ teacher_list.php
@@ -51,7 +51,6 @@
 
                 </section>
             </div>
-
                 <!----------------------------------------------- FOR MODAL --------------------------------------------------------------------->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
