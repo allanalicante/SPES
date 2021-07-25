61ccc92927f5014fe21468054df9e295446e88ed
AAlicante <alicanteallan7@gmail.com>
2021-07-25 20:56:19 +0800

    Added Login Code Query for Selecting username and Password

--- MyCrud.php
+++ MyCrud.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
+
 
 $connection = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($connection,'spes_db');
 
+/* ----------------------------------- Code to insert new level and section --------------------------------------------- */
 if(isset($_POST['insertdata']))
 {
     $level = $_POST['level'];
@@ -23,4 +26,61 @@ if(isset($_POST['insertdata']))
         echo "Data not Saved" . $query . mysqli_error($connection);
     }
 }
+/* ----------------------------------- / Code to insert new level and section /--------------------------------------------- */
+
+
+
+/* ----------------------------------- Code to login usename and password --------------------------------------------- */
+
+ elseif(isset($_POST['username']) && isset($_POST['password'])){
+
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+     $username = validate($_POST['username']);
+     $password = validate($_POST['password']);
+
+     if (empty($username)){
+         header("Location: login.php?error=Username is required!");
+         exit();
+     }
+     elseif (empty($password)){
+         header("Location: login.php?error=Password is required!");
+         exit();
+     }
+     else{
+        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $result = mysqli_query($connection,$sql);
+
+        if(mysqli_num_rows($result) === 1){
+            $row =  mysqli_fetch_assoc($result);
+            if($row['username'] === $username && $row['password'] === $password){
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header("Location: index.php");
+                exit();
+            }
+            else{
+                header("Location: login.php?error=Incorrect username or password!");
+                exit();
+            }
+        }
+        else{
+            header("Location: login.php?error=Incorrect username or password!");
+            exit();
+        }
+     }
+ }
+ else{
+     header("Location: login.php");
+     exit();
+ }
+
+
+ /* -----------------------------------/ Code to login usename and password /--------------------------------------------- */
 ?>
