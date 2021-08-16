40daadbf67515111e3a27e84b11984131f249468
AAlicante <alicanteallan7@gmail.com>
2021-08-16 13:35:36 +0800

    added a few links to css files

--- includes/head_html.php
+++ includes/head_html.php
@@ -15,8 +15,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="asset/css/bootstrap.css">
     <link rel="stylesheet" href="asset/vendors/sweetalert2/sweetalert2.min.css">
-
+    <link rel="stylesheet" href="asset/vendors/toastify/toastify.css">
     <link rel="stylesheet" href="asset/vendors/iconly/bold.css">
+    <link rel="stylesheet" href="asset/vendors/simple-datatables/style.css">
+    <link rel="stylesheet" href="asset/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="asset/css/app.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+    
+
 
     <?php
      if(isset($data)){
@@ -25,7 +31,12 @@
             case 'student-list':
             case 'section-list':
             case 'teacher-list':
-                echo '<link rel="stylesheet" href="asset/vendors/simple-datatables/style.css">';          
+                echo '<link rel="stylesheet" href="asset/vendors/simple-datatables/style.css">'; 
+                echo '<link rel="stylesheet" href="asset/vendors/sweetalert2/sweetalert2.min.css">'; 
+                echo '<link rel="stylesheet" href="asset/vendors/toastify/toastify.css">';     
+                echo '<link rel="stylesheet" href="asset/css/bootstrap.css">';     
+                echo '<link rel="stylesheet" href="asset/css/app.css">';       
+                echo '<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">';       
             break;
             }
      }
@@ -34,5 +45,7 @@
     <link rel="stylesheet" href="asset/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="asset/css/app.css">
     <link rel="shortcut icon" href="asset/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+    
 </head>
 <body>
